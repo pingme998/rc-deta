9539e8e51497dd573793be2aa3134987ff431caf
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-24 19:39:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
 os.system("curl $NAME >/home/rclone.conf")
-os.system("/home/rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --config=/home/rclone.conf")
+os.system("/home/rclone rcd --rc-serve --rc-addr=0.0.0.0:8080 --config=/home/rclone.conf")
