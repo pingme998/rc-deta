40324f620168f1e5ce08bef71e75f4d9f79682ed
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-24 17:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 import os
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-async def index():
-	name = os.getenv("NAME", "world")
-	return f"hello {name}!"
+os.system("curl $name >/rclone.conf")
